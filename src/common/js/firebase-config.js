e09f277e3bd593f5126231fe6554021cee585fa3
Emochu15912 <1001820211@unajma.edu.pe>
2025-10-22 22:36:19 -0500

    Agregado archivo .gitignore para Firebase y dependencias

--- src/common/js/firebase-config.js
+++ src/common/js/firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ9Gr6lxmRZnHB5rJ_8CvkSxg3U6PGvl4",
-  authDomain: "absolute-realm-441406-r6.firebaseapp.com",
-  databaseURL: "https://absolute-realm-441406-r6-default-rtdb.firebaseio.com",
-  projectId: "absolute-realm-441406-r6",
-  storageBucket: "absolute-realm-441406-r6.appspot.com",
-  messagingSenderId: "574988521817",
-  appId: "1:574988521817:web:37dd0cee88d2d15893bc48"
+  apiKey: "AIzaSyALeNZtkf77KHG8dJ5XQvpV-1oMxtEpcds",
+  authDomain: "grass-971ce.firebaseapp.com",
+  projectId: "grass-971ce",
+  storageBucket: "grass-971ce.firebasestorage.app",
+  messagingSenderId: "775597931199",
+  appId: "1:775597931199:web:882eae76df35e5c40f240a",
+  measurementId: "G-1VF90P2NJQ"
 };
 
 if (!firebase.apps.length) {
